c9b87c4b9148725f039761a5edd240fdd18303f5
Fabian Gans <fgans@bgc-jena.mpg.de>
2015-12-04 18:34:27 +0100

    fixed nan warningd

--- RDataCubeReader/R/ParseConfigFile.R
+++ RDataCubeReader/R/ParseConfigFile.R
@@ -9,13 +9,12 @@ parseConfigFile<-function(
   cube.config<- list()
   x=read.table(configfile,sep="=",stringsAsFactors = F,strip.white=T)
   for (i in 1:nrow(x)) {
-    if (!is.na(as.double(x[i,2]))) {
-      cube.config[[x[i,1]]]=as.double(x[i,2])
-    } else if (grepl("^datetime",x[i,2])) {
+    if (grepl("^datetime",x[i,2])) {
       cube.config[[x[i,1]]]=as.POSIXlt(strptime(x[i,2],"datetime.datetime(%Y, %m, %d, %H, %M)"))
-    }
-    else {
+    } else if (grepl("^\\D",x[i,2])) {
       cube.config[[x[i,1]]]=x[i,2]
+    } else {
+      cube.config[[x[i,1]]]=as.double(x[i,2])
     }
   }
   cube.config
