2199fc7344a0ab6b7eb254b472fbfec8af7f632f
gdkrmr <gkraemer@bgc-jena.mpg.de>
2017-11-16 17:01:47 +0100

    remove evil whitespaces

--- R/Cube.R
+++ R/Cube.R
@@ -6,7 +6,7 @@ open.cube<-function(
   ##author<<
   ##Fabian Gans, Miguel D. Mahecha, MPI BGC Jena, Germany, fgans@bgc-jena.mpg.de mmahecha@bgc-jena.mpg.de
   cube.config<-.parseConfigFile(cubepath)
-  
+
   data.dir<-file.path(cubepath,"data")
   data.dir.entries       <-list.files(data.dir)
   data.dir.entries       <-sort(data.dir.entries)
@@ -15,7 +15,7 @@ open.cube<-function(
   names(var.name.to.index)<-data.dir.entries
   firstYearOffset=(as.double(strftime(cube.config$start_time,format="%j"))-1)/cube.config$temporal_res
   return(list(base.dir=cubepath,config=cube.config,data.dir.entries=data.dir.entries,var.name.to.index=var.name.to.index,firstYearOffset=firstYearOffset))
-  ##value<< 
+  ##value<<
   ##A list a list containing some cube info, to be used for reading cube data.
 }
 
@@ -24,27 +24,27 @@ read.cube<-function(
   ##description<< Reads data from a datacube as specified by the user for a given variable, time, longitude and latitude range
   cube, ##<< Datacube handle as returned from cube.open
   variable  = cube$data.dir.entries, ##<< A list of variables to read, defaults to all available variables
-  time      = c(cube$config$start_time,cube$config$end_time-as.difftime(1,units="days")), ##<< Start and end time of the range to be read given as POSIXlt or POSIXct. Default is the whole datacube time range 
+  time      = c(cube$config$start_time,cube$config$end_time-as.difftime(1,units="days")), ##<< Start and end time of the range to be read given as POSIXlt or POSIXct. Default is the whole datacube time range
   longitude = c(-180,180), ##<< Longitude range , default is c(-180,180)
   latitude  = c(-90,90) ##<< Latitude range, default is c(-90,90)
 ) {
   ##author<<
   ##Fabian Gans, Miguel D. Mahecha, MPI BGC Jena, Germany, fgans@bgc-jena.mpg.de mmahecha@bgc-jena.mpg.de
-  
+
   if (length(longitude)== 1) longitude <- c(longitude, longitude)
   if (length(latitude) == 1) latitude  <- c(latitude, latitude)
   if (length(time)     == 1) time      <- c(time, time)
-  
+
   config  <- cube$config
 
   grid_y1 <- round((90.0 - latitude[2]) / config$spatial_res) - config$grid_y0 + 1
   grid_y2 <- round((90.0 - latitude[1]) / config$spatial_res) - config$grid_y0
   grid_x1 <- round((180.0 + longitude[1]) / config$spatial_res) - config$grid_x0 + 1
   grid_x2 <- round((180.0 + longitude[2]) / config$spatial_res) - config$grid_x0
-  
+
   if (grid_y1-1 == grid_y2) grid_y2 <- grid_y2 + 1
   if (grid_x1-1 == grid_x2) grid_x2 <- grid_x2 + 1
-  
+
   r <- .getTimesToRead(time[1],time[2],config)
   v <- r$v
   y1 <- v[1]; i1 <- v[2];
@@ -136,7 +136,7 @@ read.cube<-function(
     }
   }
   outlist
-  ##value<< 
+  ##value<<
   ##A named list where each list entry contains the data for a single variable.
 }
 
@@ -155,14 +155,14 @@ read.cube<-function(
       ISOdate(y1, 1, 1, 0) + as.difftime(seq(d1 - 1, d2, 8), units = "days")
   } else if (y2 == (y1 + 1)) {
     c(
-      ISOdate(y1, 1, 1, 0) + as.difftime(seq(d1 - 1, 365, 8), units = "days"), 
+      ISOdate(y1, 1, 1, 0) + as.difftime(seq(d1 - 1, 365, 8), units = "days"),
       ISOdate(y2, 1, 1, 0) + as.difftime(seq(0,       d2, 8), units = "days")
     )
   } else {
     c(
-      ISOdate(y1, 1, 1, 0) + as.difftime(seq(d1 - 1, 365, 8), units = "days"), 
+      ISOdate(y1, 1, 1, 0) + as.difftime(seq(d1 - 1, 365, 8), units = "days"),
       ISOdate(rep((y1 + 1):(y2 - 1), each = NpY), 1, 1, 0) +
-        as.difftime(rep(seq(0, 365, 8), (y2-y1-1)), units = "days"), 
+        as.difftime(rep(seq(0, 365, 8), (y2-y1-1)), units = "days"),
       ISOdate(y2, 1, 1, 0)+as.difftime(seq(0, d2, 8), units = "days")
     )
   }
